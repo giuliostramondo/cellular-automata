eb660758215ee4084699a5c4241d2d2e3f2a844a
Giulio Stramondo <giuliostramondo@gmail.com>
2016-04-21 15:02:25 +0200

    Initial Commit

---
+++ CellularAutomata/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CellularAutomata
+//
+//  Created by Giulio Stramondo on 21/04/16.
+//  Copyright © 2016 ArancinoProject. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CellularAutomata/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CellularAutomata
+//
+//  Created by Giulio Stramondo on 21/04/16.
+//  Copyright © 2016 ArancinoProject. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
