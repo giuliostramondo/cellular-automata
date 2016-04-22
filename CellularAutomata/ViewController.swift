40bb5d900b4a2879e5e5371bfeb4640012c08a00
Giulio Stramondo <giuliostramondo@gmail.com>
2016-04-22 15:33:23 +0200

    Factorized image width size

--- CellularAutomata/ViewController.swift
+++ CellularAutomata/ViewController.swift
@@ -22,25 +22,32 @@ class ViewController: NSViewController {
     
     var BinaryRule:[Bool] = []
     
+    var simulationWidth = 1700
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tmpAutoma = NSImage(size: NSSize(width: 1000, height: 733))
+        let tmpAutoma = NSImage(size: NSSize(width: simulationWidth, height: 733))
         tmpAutoma.lockFocus()
         NSColor.whiteColor().set()
-        NSRectFill(NSMakeRect(0, 0, 1000, 733))
+        NSRectFill(NSMakeRect(0, 0, CGFloat(simulationWidth), 733))
         tmpAutoma.unlockFocus()
+        var imageRect = NSMakeRect(0.0,0.0,tmpAutoma.size.width,tmpAutoma.size.height);
+        
+        ImageField.frame=imageRect
+        
         ImageField.image = tmpAutoma
+        
+        print(ImageField.bounds)
         ComboAutomaType.removeAllItems()
         ComboAutomaType.addItemsWithTitles(["1D-Rule 2-Colors","1D-Rule 3-Colors"])
 
-        for _ in 0...499 {
+        for _ in 0...(simulationWidth/2)-1 {
             AutomaRow.append(false)
         }
     }
 
     func drawRow(RowToDraw: [Bool], RowNumber: Int){
         if(RowNumber * 2 > 733){ return }
-        for i in 0...498{
+        for i in 0...(simulationWidth/2)-2{
             if(RowToDraw[i]){
                 NSColor.blackColor().set()
                 NSRectFill(NSMakeRect(CGFloat(i*2), CGFloat(730-(2*RowNumber)), 2, 2))
@@ -51,7 +58,7 @@ class ViewController: NSViewController {
     func computeNextRow(CurrentRow: [Bool], RuleSet: [Bool])->[Bool]{
         var newRow: [Bool] = []
         newRow.append(false)
-        for i in 1...498 {
+        for i in 1...(simulationWidth/2)-2 {
             if(CurrentRow[i-1] && CurrentRow[i] && CurrentRow[i+1]){
                 newRow.append(RuleSet[7])
                 continue
@@ -111,18 +118,18 @@ class ViewController: NSViewController {
                 Rule = getRuleSet(self.RuleField.integerValue)
                 
                 print(Rule)
-                for i in 0...499 {
-                    if (i==250){
+                for i in 0...(simulationWidth/2)-1 {
+                    if (i==(simulationWidth/4)){
                         AutomaRow[i]=true
                     }else{
                         AutomaRow[i]=false
                     }
                 }
                 
-                let newAutoma = NSImage(size: NSSize(width: 1000, height: 733))
+                let newAutoma = NSImage(size: NSSize(width: simulationWidth, height: 733))
                 newAutoma.lockFocus()
                 NSColor.whiteColor().set()
-                NSRectFill(NSMakeRect(0, 0, 1000, 733))
+                NSRectFill(NSMakeRect(0, 0, CGFloat(simulationWidth), 733))
                 drawRow(AutomaRow,RowNumber: 0)
                 for i in 1...366{
                     tmpAutomaRow = computeNextRow(AutomaRow,RuleSet: Rule)
@@ -132,10 +139,15 @@ class ViewController: NSViewController {
                 //NSColor.blackColor().set()
                 //NSRectFill(NSMakeRect(500, 723, 2, 2))
                 newAutoma.unlockFocus()
+                var imageRect = NSMakeRect(0.0,0.0,newAutoma.size.width,newAutoma.size.height);
+
+               // ImageField.frame=imageRect
                 ImageField.image=newAutoma
-            }
+                //ImageField.sizeThatFits(newAutoma.size)            }
         }
 
+    
+        }
     }
 
     override var representedObject: AnyObject? {
